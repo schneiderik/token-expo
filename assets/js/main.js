5d471144e610630508a2ea10cc052bbb231a79cd
Erik Schneider <erik.schneider41@gmail.com>
2016-06-04 12:53:08 -0500

    resize slideshows on pageload

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,32 +7,37 @@ slideshowElements.forEach(function (slideshowEl) {
   cycle.start();
 });
 
-window.addEventListener('resize', function () {
-  slideshowElements.forEach(function (slideshowEl) {
-    let containerWidth = parseInt(window.getComputedStyle(slideshowEl.parentNode).getPropertyValue('width'));
+function resizeSlideshows() {
+  slideshowElements.forEach(resizeSlideshow);
+}
 
-    let elements = Array.prototype.slice.call(slideshowEl.querySelectorAll('img'));
+function resizeSlideshow(slideshowEl) {
+  let containerWidth = parseInt(window.getComputedStyle(slideshowEl.parentNode).getPropertyValue('width'));
 
-    if (parseInt(containerWidth) < 600) {
-      let height = containerWidth * 0.666;
+  let elements = Array.prototype.slice.call(slideshowEl.querySelectorAll('img'));
 
-      slideshowEl.style.height = height + 'px';
+  if (parseInt(containerWidth) < 600) {
+    let height = containerWidth * 0.666;
 
-      elements.forEach(function (el) {
-        el.style.height = height + 'px';
-        el.style.width = containerWidth + 'px';
-        el.style.marginLeft = containerWidth / 2 * -1 + 'px';
-        el.style.marginTop = height / 2 * -1 + 'px';
-      });
-    } else {
-      slideshowEl.style.height = null;
+    slideshowEl.style.height = height + 'px';
 
-      elements.forEach(function (el) {
-        el.style.height = '400px';
-        el.style.width = '600px';
-        el.style.marginLeft = '-300px';
-        el.style.marginTop = '-200px';
-      });
-    }
-  });
-});
+    elements.forEach(function (el) {
+      el.style.height = height + 'px';
+      el.style.width = containerWidth + 'px';
+      el.style.marginLeft = containerWidth / 2 * -1 + 'px';
+      el.style.marginTop = height / 2 * -1 + 'px';
+    });
+  } else {
+    slideshowEl.style.height = null;
+
+    elements.forEach(function (el) {
+      el.style.height = '400px';
+      el.style.width = '600px';
+      el.style.marginLeft = '-300px';
+      el.style.marginTop = '-200px';
+    });
+  }
+}
+
+window.addEventListener('resize', resizeSlideshows);
+resizeSlideshows();
--- public/js/main.js
+++ public/js/main.js
@@ -77,37 +77,42 @@ slideshowElements.forEach(function (slideshowEl) {
   cycle.start();
 });
 
-window.addEventListener('resize', function () {
-  slideshowElements.forEach(function (slideshowEl) {
-    var containerWidth = parseInt(window.getComputedStyle(slideshowEl.parentNode).getPropertyValue('width'));
+function resizeSlideshows() {
+  slideshowElements.forEach(resizeSlideshow);
+}
 
-    var elements = Array.prototype.slice.call(slideshowEl.querySelectorAll('img'));
+function resizeSlideshow(slideshowEl) {
+  var containerWidth = parseInt(window.getComputedStyle(slideshowEl.parentNode).getPropertyValue('width'));
 
-    if (parseInt(containerWidth) < 600) {
-      (function () {
-        var height = containerWidth * 0.666;
+  var elements = Array.prototype.slice.call(slideshowEl.querySelectorAll('img'));
 
-        slideshowEl.style.height = height + 'px';
+  if (parseInt(containerWidth) < 600) {
+    (function () {
+      var height = containerWidth * 0.666;
 
-        elements.forEach(function (el) {
-          el.style.height = height + 'px';
-          el.style.width = containerWidth + 'px';
-          el.style.marginLeft = containerWidth / 2 * -1 + 'px';
-          el.style.marginTop = height / 2 * -1 + 'px';
-        });
-      })();
-    } else {
-      slideshowEl.style.height = null;
+      slideshowEl.style.height = height + 'px';
 
       elements.forEach(function (el) {
-        el.style.height = '400px';
-        el.style.width = '600px';
-        el.style.marginLeft = '-300px';
-        el.style.marginTop = '-200px';
+        el.style.height = height + 'px';
+        el.style.width = containerWidth + 'px';
+        el.style.marginLeft = containerWidth / 2 * -1 + 'px';
+        el.style.marginTop = height / 2 * -1 + 'px';
       });
-    }
-  });
-});
+    })();
+  } else {
+    slideshowEl.style.height = null;
+
+    elements.forEach(function (el) {
+      el.style.height = '400px';
+      el.style.width = '600px';
+      el.style.marginLeft = '-300px';
+      el.style.marginTop = '-200px';
+    });
+  }
+}
+
+window.addEventListener('resize', resizeSlideshows);
+resizeSlideshows();
 
 },{"./cycle":1}]},{},[2])(2)
 });
